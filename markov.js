826229c63a0c8292ffbbc2b65d0b715cafe5ac45
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-03-23 16:18:51 -0700

    first commit

---
+++ markov.js
@@ -0,0 +1,69 @@
+/** Textual markov chain generator */
+
+
+class MarkovMachine {
+
+  /** build markov machine; read in text.*/
+
+  constructor(text) {
+    let words = text.split(/[ \r\n]+/);
+    // MORE CODE HERE
+    this.words = words
+  }
+
+  /** set markov chains:
+   *
+   *  for text of "the cat in the hat", chains will be
+   *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
+
+  makeChains() {
+    // MORE CODE HERE
+    markovObj = {};
+    for (let i = 0; i < this.words.length; i++) {
+      if (i == this.words.length - 1) {
+        if (!markovObj[this.words[i]]) {
+          markovObj[this.words[i]] = [null];
+        } else {
+          markovObj[this.words[i]].push(null);
+        }
+      } else if (!markovObj[this.words[i]]) {
+        markovObj[this.words[i]] = [this.words[i+1]];
+      } else {
+        markovObj[this.words[i]].push(this.words[i+1]);
+      }
+    }
+    return markovObj;
+  }
+
+
+  /** return random text from chains */
+
+  getText(numWords = 100) {
+    // MORE CODE HERE
+    let markovArray = [];
+    let markovObj = this.makeChains();
+    let randKey = startRandomWord(markovObj);
+    while( markovArray.length < numWords) {
+      let randVal = getRandomWordInArray(markovObj, randKey)
+      markovArray.push(randVal);
+      randKey = randVal;
+    }
+    return markovArray.join(" ")
+  }
+}
+
+function startRandomWord(obj) {
+  let objKeys = Object.keys(obj) 
+
+  return objKeys[Math.floor(Math.random * objKeys.length)]
+
+} 
+
+function getRandomWordInArray(obj, randomKey) {
+  let lengthOfValueArray = obj[randomKey].length;
+  let randVal = obj[randomKey][Math.floor(Math.random * lengthOfValueArray)]
+
+  return randVal
+}
+
+
